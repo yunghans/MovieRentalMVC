5dac0c5148adc4b24f7c194b404015bde0446985
Yunghans Irawan <yunghans@hotmail.com>
2014-04-14 16:19:24 +0800

    Exercise 4 Solution

---
+++ MovieRental/Areas/Admin/Controllers/BulkLoadController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+using System.Text.RegularExpressions;
+using MovieRental.Externals;
+using MovieRental.Data;
+
+namespace MovieRental.Areas.Admin.Controllers
+{
+    public class BulkLoadController : Controller
+    {
+        //
+        // GET: /Admin/BulkLoad/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string movieList, 
+            double defaultRentalRate, int defaultQty)
+        {
+            string[] titles = titles = Regex.Split(movieList, "\r\n|\r|\n");
+
+            List<string> unresolvedTitles = new List<string>();
+            List<Movie> resolvedMovie = new List<Movie>();
+
+            foreach (string title in titles)
+            {
+                Movie m = OMDBService.GetMoviesByTitle(title);
+                if (m != null)
+                {
+                    m.RentalRate = defaultRentalRate;
+                    m.NoInStock = defaultQty;
+                    resolvedMovie.Add(m);
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(title))
+                        unresolvedTitles.Add(title);
+                }
+            }
+
+            BulkLoadOutput blo = 
+                new BulkLoadOutput(unresolvedTitles, resolvedMovie);
+
+            TempData["blo"] = blo;
+            return View("BulkOutput", blo);
+        }
+
+        [HttpPost]
+        public ActionResult Save(string save)
+        {
+            List<string> messages = new List<string>();
+            if (save == "Yes")
+            {
+                BulkLoadOutput blo = (BulkLoadOutput)TempData["blo"];
+                if (blo != null)
+                {
+                    MovieRepository rep = new MovieRepository();
+
+                    foreach (Movie m in blo.ResolvedMovie)
+                    {
+                        try
+                        {
+                            rep.Add(m);
+                            rep.Save();
+                            messages.Add("Successfully add movie " + m.Title);
+                        }
+                        catch
+                        {
+                            messages.Add("Unable to add movie " + m.Title
+                                + ". Perhaps it is already in the database.");
+                        }
+                    }
+                }
+                else
+                {
+                    messages.Add("No movies loaded.");
+                }
+            }
+            else
+            {
+                messages.Add("Bulk Load operation is successfully cancelled.");
+            }
+            return View(messages);
+        }
+
+    }
+
+
+    public class BulkLoadOutput
+    {
+        public BulkLoadOutput(List<string> unresolvedTitles, List<Movie> resolvedMovie)
+        {
+            UnresolvedTitles = unresolvedTitles;
+            ResolvedMovie = resolvedMovie;
+        }
+        public List<string> UnresolvedTitles { get; set; }
+        public List<Movie> ResolvedMovie { get; set; }
+    }
+}
---
+++ MovieRental/Models/OMDBService.cs
@@ -0,0 +1,108 @@
+﻿using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using MovieRental.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+
+namespace MovieRental.Externals
+{
+    public class OMDBService
+    {
+        public static Movie GetMoviesByTitle(string Title)
+        {
+            WebRequest req = WebRequest.Create(@"http://www.omdbapi.com/?t=" + Title);
+
+            req.Method = "GET";
+            
+            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(MovieModel));
+                object objMovieModel = jsonSerializer.ReadObject(resp.GetResponseStream());
+                MovieModel jsonMovieModel = objMovieModel as MovieModel;
+                Mapper.CreateMap<MovieModel, Movie>();
+
+                //Do some filtering
+                if (String.IsNullOrEmpty(jsonMovieModel.Title) || jsonMovieModel.Poster == "N/A")
+                {
+                    return null;
+                }
+                else
+                {
+                    Movie mappedMovie = Mapper.Map<Movie>(jsonMovieModel);
+
+                    List<Genre> genres = new List<Genre>();
+                    foreach (string aGenre in jsonMovieModel.Genre.Split(','))
+                    {
+                        genres.Add(new Genre(aGenre.Trim()));
+                    }
+                    mappedMovie.Genres = genres;
+
+                    return mappedMovie;
+                }
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    [DataContract]
+    public class MovieModel
+    {
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public string Year { get; set; }
+        [DataMember(Name = "Rated")]
+        public string FilmRating { get; set; }
+        [DataMember]
+        public string Released { get; set; }
+        public string ReleaseYear
+        {
+            get
+            {
+                try
+                {
+                    return DateTime.Parse(Released).Year.ToString();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+        [DataMember]
+        public string Runtime { get; set; }
+        [DataMember]
+        public string Genre { get; set; }
+        [DataMember]
+        public string Actors { get; set; }
+        [DataMember]
+        public string Plot { get; set; }
+        [DataMember]
+        public string Language { get; set; }
+        [DataMember]
+        public string Poster { get; set; }
+        [DataMember]
+        public string imdbRating { get; set; }
+        [DataMember]
+        public string imdbID { get; set; }
+
+        public string MovieId
+        {
+            get
+            {
+                return imdbID;
+            }
+        }
+        public override string ToString()
+        {
+            return Title + " " + Year + " " + FilmRating;
+        }
+    }
+}
